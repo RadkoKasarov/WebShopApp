f30f44bdcf686606733ee47de7e21974199bd34c
RadkKasarov <r.kasarov2021@pmg-pernik.com>
2025-12-18 12:47:10 +0200

    second commit

---
+++ WebShopApp/WebShopApp/Models/Product/ProductCreateVM.cs
@@ -0,0 +1,25 @@
+﻿using NuGet.Protocol;
+
+using System.ComponentModel.DataAnnotations;
+
+using WebShopApp.Models.Brand;
+
+namespace WebShopApp.Models.Product
+{
+    public class ProductCreateVM
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Brand")]
+        public int CategoryId { get; set; }
+        public virtual List<BrandPairVM> Categories { get; set; } = new List<BrandPairVM>();
+
+
+    }
+}
